03d4149b4ba0654d94a8fe7e63d88086b7903dd5
vamandeshmukh <87980161+shridhar-javafsd@users.noreply.github.com>
2022-02-04 15:53:07 +0530

    js updated

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+import { express } from 'express';
 
-// js code here 
+const exp = express();
+const port = 9999;
+
+// exp.get(arg1, arg2);
+// exp.get('/', () => {});
+
+exp.get('/', (response, request) => {
+    response.send(`Welcome to SFDC app at ${port}.`);
+    console.log(`Welcome!`)
+});
+
+// exp.listen(arg1, arg2);
+// exp.listen(port, () => {});
+
+exp.listen(port, () => {
+    console.log(`My app is running at ${port}.`)
+});
+
