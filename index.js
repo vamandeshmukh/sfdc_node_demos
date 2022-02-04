abd019c8df6ef9802743ba09b3299014a7f446d5
vamandeshmukh <87980161+shridhar-javafsd@users.noreply.github.com>
2022-02-04 16:55:25 +0530

    js updated

--- index.js
+++ index.js
@@ -2,20 +2,35 @@
 // import { express } from 'express';
 const express = require('express');
 const exp = express();
+
 const port = 9999;
 
 // to run this app - 
 // npm start 
 // view the output on browser at 
 // http://localhost:9999/
+// to stop - 
+// Ctrl + C
+// Y 
 
 // exp.get(arg1, arg2);
 // exp.get('/', () => {});
 
 exp.get('/', (request, response) => {
     response.send(`Welcome to SFDC app at ${port}`);
-    console.log(`Welcome!`)
+    console.log(`Welcome!`);
 });
+
+exp.get('/home', (request, response) => {
+    response.send(`Home page of SFDC app at ${port}`);
+    console.log(`home`);
+});
+
+exp.get('/company', (request, response) => {
+    response.send(`Company page of SFDC app at ${port}`);
+    console.log(`company`);
+});
+
 
 // exp.listen(arg1, arg2);
 // exp.listen(port, () => {});
